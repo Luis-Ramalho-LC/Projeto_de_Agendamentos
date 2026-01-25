2f7f487a99484ac196333926ee0fd16f7ade8dec
Luís Ramalho <luisramalholc@gmail.com>
2026-01-25 20:45:20 -0300

    Implementação do método http POST, e modificação dos arquivos JSON

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const APIServicoUrl = 'http://localhost:3000/servicos';
+
 document.addEventListener('DOMContentLoaded', function() {
         const calendarEl = document.getElementById('calendar')
         const calendar = new FullCalendar.Calendar(calendarEl, {
@@ -8,20 +10,32 @@ document.addEventListener('DOMContentLoaded', function() {
           center: 0,
           right: 'title'
           },
-          events: 'agendamentos.json'
+          events: 'http://localhost:3000/agendamentos'
         })
         calendar.render()
       })
 
-fetch('servicos.json', { method: 'GET' })
+fetch(APIServicoUrl, { method: 'GET' })
 .then(function(resposta){ resposta.json()
   .then(function(verServicos){ verServicos.forEach(function(servico){
-    document.getElementById("TiposDeServiços").innerHTML += "<div>" + servico.nome
+    document.getElementById("TiposDeServiços").innerHTML += 
+      "Tipo de Serviço: " + servico.nomeDoServico +
+      "<br>Tempo Gasto: " + servico.tempoGastoServico + " Minutos" +
+      "<br> Preço do Serviço: " + servico.precoDoServico + " R$"
   })
  }) 
 })
 
-fetch('servicos.json', {
-  method: "POST",
-  body: JSON.stringify(novoServico)
-})
+function salvarNovoServico(){
+  var servico = [];
+  const nomeDoServico = document.getElementById("nomeServico").value;
+  const tempoGastoServico = document.getElementById("tempoGasto").value;
+  const precoDoServico = document.getElementById("preco").value;
+  servico = {nomeDoServico, tempoGastoServico, precoDoServico};
+
+  fetch(APIServicoUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(servico)
+  })
+}
